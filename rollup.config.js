78fbb0d2a4f2df57a2247bedb7328b2b70200045
Jochen Görtler <web@goertler.email>
2019-08-12 10:01:05 -0600

    Include source maps 🗺

--- rollup.config.js
+++ rollup.config.js
@@ -12,10 +12,12 @@ export default {
         {
             file: pkg.main,
             format: 'cjs',
+            sourcemap: true,
         },
         {
             file: pkg.module,
             format: 'es',
+            sourcemap: true,
         },
     ],
     external: [
