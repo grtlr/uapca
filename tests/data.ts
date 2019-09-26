45f3e33fbcc1a0da6bb8c836a1713cb08b3edea2
Jochen Görtler <jochen.goertler@uni-konstanz.de>
2019-09-26 13:24:43 +0200

    Formatting

--- tests/data.ts
+++ tests/data.ts
@@ -1,20 +1,18 @@
-import { MultivariateNormal, UaPCA } from '../src/index.ts';
-import { describe, it } from 'mocha';
+import * as studentGrades from '../data/student_grades.json';
 
+import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import { Matrix } from 'ml-matrix';
-
-import * as student_grades from '../data/student_grades.json';
+import { MultivariateNormal } from '../src/index.ts';
 
 describe('Student dataset', () => {
-    it('should find the correct projection', () => {
+    const dists = studentGrades.distributions.map(d => new MultivariateNormal(
+        new Matrix(d.mean),
+        new Matrix(d.cov)
+    ));
 
-       let dists = student_grades.distributions.map(d => new MultivariateNormal(
-           new Matrix(d.meanVec),
-           new Matrix(d.covMat)
-       ));
-
-       expect(dists.length).to.eql(6);
-       expect(dists[0].mean()).to.eql(Matrix.columnVector([15, 12.29, 14.1, 15]));
+    it('should load the dataset', () => {
+        expect(dists.length).to.eql(6);
+        expect(dists[0].mean()).to.eql(Matrix.columnVector([15, 12.29, 14.1, 15]));
     });
 });
