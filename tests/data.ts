c44a87fb69cd3dcf1bee8bf764f4b0024218a7da
Jochen Görtler <jochen.goertler@uni-konstanz.de>
2019-10-04 13:04:23 +0200

    Convert dataset to rows

--- tests/data.ts
+++ tests/data.ts
@@ -3,16 +3,20 @@ import * as studentGrades from '../data/student_grades.json';
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import { Matrix } from 'ml-matrix';
-import { MultivariateNormal } from '../src/index.ts';
+import { MultivariateNormal, UaPCA } from '../src/index.ts';
 
 describe('Student dataset', () => {
-    const dists = studentGrades.distributions.map(d => new MultivariateNormal(
-        new Matrix(d.mean),
-        new Matrix(d.cov)
-    ));
+    const dists = studentGrades.distributions.map(d => new MultivariateNormal(d.mean, d.cov));
 
     it('should load the dataset', () => {
         expect(dists.length).to.eql(6);
-        expect(dists[0].mean()).to.eql(Matrix.columnVector([15, 12.29, 14.1, 15]));
+        expect(dists[0].mean()).to.eql(Matrix.rowVector([15, 12.285714285714285, 14.095238095238097, 15]));
+    });
+
+    it('should work with UaPCA', () => {
+        const pca = UaPCA.fit(dists);
+        const transformed = pca.transform(dists, 2);
+        expect(transformed.length).to.eql(6);
+        expect(transformed[0].mean().columns).to.eql(2);
     });
 });
